9da05eb3afa20fa3db08f3e7ce1497d04273c466
Serhii Aif <sergiyayf@gmail.com>
2023-06-01 14:57:16 +0200

    add sphinx to setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ setup(
                       'pandas>=1.5.2', 'seaborn>=0.12.1',
                       'gymnasium==0.28.1', 'stable-baselines3[extra]>=2.0.0a10',
                       'pyzmq>=24.0.1', 'pyyaml>=5.4.1', 'tensorboard>=2.7.0', 'dvc', 'click',
-                      'scipy', 'pymc>=4.0.1', 'sb3-contrib', 'ruff', 'pytest', 'pytest-cov'
+                      'scipy', 'pymc>=4.0.1', 'sb3-contrib', 'ruff', 'pytest', 'pytest-cov',
+                      'sphinx', 'sphinx_rtd_theme'
                       ],
     python_requires=">=3.9",
     classifiers=['Operating System :: ubuntu',
