319f3ee14493b92e20b79cb9db6833e77bb03738
Serhii Aif <sergiyayf@gmail.com>
2023-08-18 13:52:23 +0200

    update sandbox plotting of the evaluations, run few more LV evals

--- sandbox.py
+++ sandbox.py
@@ -5,7 +5,7 @@ mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
-def get_ttps(filename, timesteps=40):
+def get_ttps(filename, timesteps=100):
     ttps = []
     for i in range(timesteps):
         df = pd.read_hdf(filename, key='run_'+str(i))
@@ -17,55 +17,64 @@ def get_ttps(filename, timesteps=40):
             ttps.append(0)
     return ttps
 
-# Loop through every key in the file
-ttps_no_treat = get_ttps('Evaluations/PcEnvEvalpatient_80_no_treatment.h5')
-ttps_mtd = get_ttps('Evaluations/PcEnvEvalpatient_80_mtd.h5')
-ttps_random = get_ttps('Evaluations/PcEnvEvalpatient_80_random.h5')
-ttps_at100 = get_ttps('Evaluations/PcEnvEvalpatient_80_AT_at_baseline.h5')
-ttps_at50 = get_ttps('Evaluations/PcEnvEvalpatient_80_AT50.h5')
-ttps_at75 = get_ttps('Evaluations/PcEnvEvalpatient_80_AT75.h5')
-ttps_LV_at = get_ttps('Evaluations/LvEnvEvalpatient_80_lv_fixed_cap_test.h5')
-ttps_LV_no_treat = get_ttps('Evaluations/LvEnvEvalLV-no_treat.h5')
-ttps_LV_mtd = get_ttps('Evaluations/LvEnvEvalLV-mtd.h5')
-ttps_LV_random = get_ttps('Evaluations/LvEnvEvalLV-random.h5')
-ttps_PC_rl = get_ttps('Evaluations/PcEnvEvalpatient_80_RL_training_images_1407.h5')
-ttps_PC_rl_new = get_ttps('Evaluations/PcEnvEvalpatient_80_RL_new_1407.h5')
+PC_files_list = ['Evaluations/PcEnvEvalpatient_80_no_treatment.h5',
+                    'Evaluations/PcEnvEvalpatient_80_mtd.h5',
+                    'Evaluations/PcEnvEvalpatient_80_AT50.h5',
+                    'Evaluations/PcEnvEvalpatient_80_AT75.h5',
+                    'Evaluations/PcEnvEvalpatient_80_AT100.h5',
+                    'Evaluations/PcEnvEvalpatient_80_random.h5',
+                 ]
+PC_name_lsit = ['PC No treatment', 'PC MTD',
+                'PC AT50', 'PC AT75', 'PC AT100', 'PC Random']
 
-ttps_agent = get_ttps('Evaluations/PcEnvEval1008_interruption_with_57.h5')
+LV_files_list = ['Evaluations/LvEnvEvalLV-no_treat.h5',
+                    'Evaluations/LvEnvEvalLV-mtd.h5',
+                    'Evaluations/LvEnvEvalLV-AT50.h5',
+                    'Evaluations/LvEnvEvalLV-AT75.h5',
+                    'Evaluations/LvEnvEvalLV-AT100.h5',
+                    'Evaluations/LvEnvEvalLV-random.h5',
+                 ]
+LV_name_lsit = ['LV No treatment', 'LV MTD',
+                'LV AT50', 'LV AT75', 'LV AT100', 'LV Random']
 
-df = pd.DataFrame({'MTD': ttps_mtd,
-                   'LV_MTD': ttps_LV_mtd,
-                   'AT100': ttps_at100,
-                   'LV_AT': ttps_LV_at,
-                   'Random': ttps_random,
-                   'LV_Random': ttps_LV_random,
-                   'RL treat PC': ttps_PC_rl_new,
-                   } )
+PC_dict = {}
+LV_dict = {}
+combined = {}
+for i in range(len(PC_files_list)):
+    PC_dict[PC_name_lsit[i]] = get_ttps(PC_files_list[i])
 
-df_ats = pd.DataFrame({'AT50': ttps_at50,
-                       'AT75': ttps_at75,
-                       'AT100': ttps_at100,
-                       'agent': ttps_agent,
-                       'LV_AT100': ttps_LV_at,
-                       } )
+for i in range(len(LV_files_list)):
+    LV_dict[LV_name_lsit[i]] = get_ttps(LV_files_list[i])
+
+for i in range(len(PC_files_list)):
+    combined[PC_name_lsit[i]] = get_ttps(PC_files_list[i])
+    combined[LV_name_lsit[i]] = get_ttps(LV_files_list[i])
+
+PC_df = pd.DataFrame(PC_dict)
+LV_df = pd.DataFrame(LV_dict)
+combined_df = pd.DataFrame(combined)
 
 # box plot the distribution with scatter using seaborn
 fig, ax = plt.subplots()
-sns.boxplot(data=df, ax=ax)
-sns.stripplot(data=df, ax=ax, color='black', jitter=0.2, size=2.5)
-ax.scatter(df.mean().index, df.mean().values, marker='o', color='red', s=20)
-
-# box plot the distribution with scatter using seaborn
-fig, ax = plt.subplots()
-sns.boxplot(data=df_ats, ax=ax)
-sns.stripplot(data=df_ats, ax=ax, color='black', jitter=0.2, size=2.5)
-ax.scatter(df_ats.mean().index, df_ats.mean().values, marker='o', color='red', s=20)
-
+sns.boxplot(data=PC_df, ax=ax)
+sns.stripplot(data=PC_df, ax=ax, color='black', jitter=0.2, size=2.5)
 # show mean as well
+ax.scatter(PC_df.mean().index, PC_df.mean(), marker='x', color='red', s=50, label='mean')
 
+fig, ax = plt.subplots()
+sns.boxplot(data=LV_df, ax=ax)
+sns.stripplot(data=LV_df, ax=ax, color='black', jitter=0.2, size=2.5)
+# show mean as well
+ax.scatter(LV_df.mean().index, LV_df.mean(), marker='x', color='red', s=50, label='mean')
+
+fig, ax = plt.subplots()
+sns.boxplot(data=combined_df, ax=ax)
+sns.stripplot(data=combined_df, ax=ax, color='black', jitter=0.2, size=2.5)
+# show mean as well
+ax.scatter(combined_df.mean().index, combined_df.mean(), marker='x', color='red', s=50, label='mean')
 
 # plot one trajectory of aT scenario
-df = pd.read_hdf('Evaluations/PcEnvEvalpatient_80_AT50.h5', key='run_20')
+df = pd.read_hdf('Evaluations/PcEnvEvalpatient_80_AT100.h5', key='run_40')
 fig, ax = plt.subplots()
 ax.plot(df.index, df['Type 0'], label='Type 0')
 ax.plot(df.index, df['Type 1'], label='Type 1')
