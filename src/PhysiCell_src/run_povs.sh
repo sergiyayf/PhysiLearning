f48bb6d74d6ebc14f6731e0e5b5ad7707f6750c8
Serhii Aif <sergiyayf@gmail.com>
2023-10-23 15:52:33 +0200

    add good black and white povwriters

--- src/PhysiCell_src/run_povs.sh
+++ src/PhysiCell_src/run_povs.sh
@@ -3,7 +3,7 @@
 count=$(ls -1 ./output/*.svg 2>/dev/null | wc -l);
 
 echo "number of files is $count" 
-./povwriter 0:1:20;
+./povwriter_white_background 0:1:$count;
 for scene in *.pov; do povray +w640 +h640 +a0.3 -D $scene; done ;
 ffmpeg -r 8 -f image2 -i ./pov%08d.png -vcodec libx264 -pix_fmt yuv420p -strict -2 -tune animation -crf 15 -acodec none ./out.mp4
 
