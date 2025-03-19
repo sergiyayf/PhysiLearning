fix first observation normalization after reset

kpp_sandbox.py
@@ -11,27 +11,35 @@ import numpy as np
 # ax.plot(time, df['Type 0'], c='blue')
 # ax.plot(time, df['Type 1'], c='orange')
 # ax.set_yscale('linear')
-
-fig, ax = plt.subplots(10,1)
-for i in range(10):
-    df = pd.read_hdf('./Evaluations/KppEnvEval_ttp_1.h5', key='run_'+str(i))
+runs = 1
+fig, ax = plt.subplots(runs, 1)
+for i in range(runs):
+    df = pd.read_hdf('./Evaluations/KppEnvEval_2025_03_18_lin_2_2.h5', key='run_'+str(i))
     day = 400
     time = np.arange(0, len(df['Type 0']))/day
     time = time[::day]
     sens = np.array(df['Type 0'][::day])
     res = np.array(df['Type 1'][::day])
     tot = sens+res
+    tot = tot
     treat = np.array(df['Treatment'][::day])
 
-    ax[i].plot(time, sens, c='blue')
-    ax[i].plot(time, res, c='orange')
-    ax[i].plot(time, tot, '--', c='k')
-    ax[i].set_yscale('linear')
-    ax[i].axhline(y=2.5, color='k')
+    if runs == 1:
+        a = ax
+    else:
+        a = ax[i]
+    a.plot(time, sens, c='blue')
+    a.plot(time, res, c='orange')
+    a.plot(time, tot, '--', c='k')
+    a.set_yscale('linear')
+    a.axhline(y=3.0, color='k')
+    a.axhline(y=1.0, color='k')
+    a.set_ylim(0, 4)
+    a.set_xlim(0, 60)
     #fill_between treatment
     for t in range(len(time)-1):
         if treat[t] == 1:
-            ax[i].axvspan((t-1), t, color='grey', alpha=0.5)
+            a.axvspan((t-1), t, color='grey', alpha=0.5)
     #ax[i].fill_between(time, 0, 3, where=treat==1, color='gray', alpha=0.5)
 
 # read npy
@@ -47,5 +55,5 @@ for i in range(10):
 # ax[0,1].plot(s_1, c='blue')
 # ax[0,1].plot(r_1, c='orange')
 # ax[0,1].set_yscale('linear')
-
+fig.savefig('ttp_1_2.pdf', transparent=True)
 plt.show()

src/physilearning/envs/kpp_env.py
@@ -243,14 +243,7 @@ class KppEnv(BaseEnv):
         self.initialize_population()
         self.initial_wt = np.sum(self.sensitive_population * 2 * np.pi * self.radius_array)
         self.initial_mut = np.sum(self.resistant_population * 2 * np.pi * self.radius_array)
-        if self.normalize:
-            self.normalization_factor = self.normalize_to / (self.initial_wt + self.initial_mut)
-            self.initial_wt *= self.normalization_factor
-            self.initial_mut *= self.normalization_factor
-            self.threshold_burden = self.max_tumor_size * self.normalize_to
-        self.state = [self.initial_wt, self.initial_mut, 0]
-        self.trajectory = np.zeros((np.shape(self.state)[0], int(self.max_time) + 1))
-        self.trajectory[:, 0] = self.state
+
         self.time = 0
         self.done = False
 
@@ -259,14 +252,14 @@ class KppEnv(BaseEnv):
         self.density_trajectory[:, 0, 1] = self.resistant_population
 
         self.burden = np.sum(self.state[0:2])
-
+        self.trajectory = np.zeros((np.shape(self.state)[0], int(self.max_time) + 1))
         for t in range(0, int(self.treatment_time_step/2)):
             # step time
             self.time += 1
             self.state[2] = 0
             s,r = self.grow()
-            self.state[0] = s*self.normalization_factor
-            self.state[1] = r*self.normalization_factor
+            self.state[0] = s#*self.normalization_factor
+            self.state[1] = r#*self.normalization_factor
             self.burden = np.sum(self.state[0:2])
 
             self.trajectory[:, self.time] = self.state
@@ -275,6 +268,17 @@ class KppEnv(BaseEnv):
             # check if done
             if self.state[0] <= 0 and self.state[1] <= 0:
                 self.state = [0, 0, 0]
+
+        if self.normalize:
+            self.normalization_factor = self.normalize_to / (self.state[0] + self.state[1])
+            self.initial_wt *= self.normalization_factor
+            self.initial_mut *= self.normalization_factor
+            self.threshold_burden = self.max_tumor_size * self.normalize_to
+
+        self.trajectory *= self.normalization_factor
+        self.state[0] = self.state[0]*self.normalization_factor
+        self.state[1] = self.state[1]*self.normalization_factor
+        self.trajectory[:, 0] = [self.initial_wt, self.initial_mut, 0]
 
         self.reward = 0
         if self.observation_type == 'number':